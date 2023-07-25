f44f0d7cfa3a3f3d89de158bf5825237f5cfab86
ezen <ezen@DESKTOP-O03MT84>
2023-07-25 11:10:33 +0900

    충돌 테스트

--- demo2/src/main/java/com/ez/demo/controller/HomeController.java
+++ demo2/src/main/java/com/ez/demo/controller/HomeController.java
@@ -5,9 +5,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-	@RequestMapping("/")
+   @RequestMapping("/")
     public String index() { 
-		System.out.println("index page!");
+      System.out.println("index page!");
+      System.out.println("담당자");
         return "index";
+    }
+   
+   @RequestMapping("/test1")
+    public String test1() { 
+      System.out.println("test1 page!");
+        return "test1";
     }
 }
